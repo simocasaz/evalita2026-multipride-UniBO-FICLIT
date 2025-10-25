ee0b8e9367be8693dad4e07780e4906eb44639ed
Simone Casazza <simone.casazza94@gmail.com>
2025-10-25 19:17:27 +0200

    feat: add functions for tokenizer loading and tokenization

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,17 @@
+from transformers import AutoTokenizer
 
+
+def load_tokenizer(model_name: str = "Musixmatch/umberto-commoncrawl-cased-v1"):
+    """
+    Loads and returns the tokenizer.
+    """
+    return AutoTokenizer.from_pretrained(model_name)
+
+
+def tokenize_batch(batch, tokenizer, max_length: int = 128):
+    """
+    Tokenizes a batch of text samples for use with 🤗 Datasets map().
+    """
+    return tokenizer(
+        batch["text"], padding="max_length", truncation=True, max_length=max_length
+    )
