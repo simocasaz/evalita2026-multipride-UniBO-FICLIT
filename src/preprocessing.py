b19aceb92cfd469c480df48fbb572db589a11790
Simone Casazza <simone.casazza94@gmail.com>
2025-10-27 11:33:07 +0100

    feat: remove max length padding from tokenization function

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -12,6 +12,4 @@ def tokenize_batch(batch, tokenizer, max_length: int = 128):
     """
     Tokenizes a batch of text samples for use with 🤗 Datasets map().
     """
-    return tokenizer(
-        batch["text"], padding="max_length", truncation=True, max_length=max_length
-    )
+    return tokenizer(batch["text"], truncation=True)
