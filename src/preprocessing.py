76561903e168d5f8c80b25ca11facc8d6a30b513
Simone Casazza <simone.casazza94@gmail.com>
2025-10-28 11:49:59 +0100

    fix: remove truncation=True from tokenize

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,8 +8,8 @@ def load_tokenizer(model_name: str = "Musixmatch/umberto-commoncrawl-cased-v1"):
     return AutoTokenizer.from_pretrained(model_name)
 
 
-def tokenize_batch(batch, tokenizer, max_length: int = 128):
+def tokenize_batch(batch, tokenizer):
     """
     Tokenizes a batch of text samples for use with 🤗 Datasets map().
     """
-    return tokenizer(batch["text"], truncation=True)
+    return tokenizer(batch["text"])
