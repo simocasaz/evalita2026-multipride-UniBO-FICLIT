eef3ab7ce4248d4194a78224eb64aac54098734e
Simone Casazza <simone.casazza94@gmail.com>
2025-11-15 13:13:59 +0100

    fix: remove log from evaluation

--- src/multipride/evaluation.py
+++ src/multipride/evaluation.py
@@ -6,8 +6,6 @@ def compute_metrics(eval_preds):
     clf_metrics = evaluate.combine(["f1", "precision", "recall"])
     logits, labels = eval_preds
     predictions = np.argmax(logits, axis=-1)
-    print("Predictions:", predictions[:10])
-    print("Labels:", labels[:10])
     return clf_metrics.compute(
         predictions=predictions, references=labels, average="macro"
     )
