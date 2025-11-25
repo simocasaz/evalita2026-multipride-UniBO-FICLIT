0728f7d02237d7d9fe868ecad2b10032436dc653
Simone Casazza <simone.casazza94@gmail.com>
2025-11-25 17:50:23 +0100

    fix: fix wrong column name for bios in tokenization function

--- src/multipride/preprocessing.py
+++ src/multipride/preprocessing.py
@@ -52,5 +52,5 @@ def tokenize_batch(batch, tokenizer):
 
 def tokenize_batch_with_bios(batch, tokenizer):
     return tokenizer(
-        batch["text"], batch["user_bio"], truncation="only_second", max_length=256
+        batch["text"], batch["bio"], truncation="only_second", max_length=256
     )
