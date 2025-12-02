c2f29006037ec3e9f86008d04c04e31e6bb8ce18
Simone Casazza <simone.casazza94@gmail.com>
2025-12-02 14:33:06 +0100

    fix: fix type error caused by compute_metrics
    Use evaluation.load to load each single metric and merge final results instead of using evaluate.combine. The latter cause an error because the argument zero_division is passed to all the metrics but f1 does not support it.

--- src/multipride/evaluation.py
+++ src/multipride/evaluation.py
@@ -3,9 +3,15 @@ import numpy as np
 
 
 def compute_metrics(eval_preds):
-    clf_metrics = evaluate.combine(["f1", "precision", "recall"])
     logits, labels = eval_preds
     predictions = np.argmax(logits, axis=-1)
-    return clf_metrics.compute(
+    f1 = evaluate.load("f1").compute(
+        predictions=predictions, references=labels, average="macro"
+    )
+    precision = evaluate.load("precision").compute(
         predictions=predictions, references=labels, average="macro", zero_division=0
     )
+    recall = evaluate.load("recall").compute(
+        predictions=predictions, references=labels, average="macro", zero_division=0
+    )
+    return {**f1, **precision, **recall}
